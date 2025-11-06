b900900f2e0bfe1437c0749a588682ed8b336b87
Ngo Nam Khanh <nam.khanh.ngo@tul.cz>
2025-11-06 09:56:26 +0100

    ukoly (zbytek)

---
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -0,0 +1,45 @@
+﻿﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************");
+    Console.WriteLine("***** Vykreslení obdélníku *****");
+    Console.WriteLine("********************************");
+    Console.WriteLine("********* Ngo Nam Khánh **********");
+    Console.WriteLine("********************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte šířku obrazce (celé číslo): ");
+    int width;
+
+    while (!int.TryParse(Console.ReadLine(), out width))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte šířku obrazce znovu: ");
+    }
+
+    Console.Write("Zadejte výška obrazce (celé číslo): ");
+    int height;
+
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte výšku obrazce znovu: ");
+    }
+
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= width; j++)
+        {
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(150)); // zpoždění
+        }
+        Console.WriteLine();
+    }
+        
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+
+}
